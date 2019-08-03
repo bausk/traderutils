9beb33e7470ed89de0b2d5011c37de73c622c51e
Alex Bausk <bauskas@gmail.com>
2019-08-03 17:52:39 +0300

    [SETUP] initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="traderutils",
+    version="0.0.1",
+    author="Oleksander Bausk",
+    author_email="bauskas@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bausk/traderutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
